79c63bf5e70e478465e93c48475d296f28c1ee55
Felice Forby <feliceforby@gmail.com>
2018-07-22 22:31:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,16 +29,16 @@ class ApplicationController < Sinatra::Base
     erb :'recipes/show'
   end
 
-  get '/recipes/:id/edit' do
-    @recipe = Recipe.find(params[:id])
-
-    erb :edit
-  end
-
   patch 'recipes/:id' do
     @recipe = Recipe.find(params[:id])
     @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time = params[:cook_time])
 
     redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+
+    erb :edit
   end
 end
